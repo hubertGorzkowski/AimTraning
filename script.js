0e47e4b8584f3104d6a2eae19a359f3ee8800892
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-01-12 21:57:10 +0100

    add correct section replacement, style sec .game,
    edit dot appearance, style sec .result

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ document.onselectstart = function () {
 };
 const startBtn = document.body.querySelector(".menu__btn--start");
 const menu = document.body.querySelector(".menu");
+const gameSection = document.querySelector(".game");
+const result = document.querySelector(".result");
 const html = document.querySelector("html");
 const pointsNumber = document.querySelector(".game__statistics--points");
 const targetsNumber = document.querySelector(".game__statistics--targets");
+let difficultyLevel = "normal";
 
 let counter = 0;
 let activeElements = 0;
@@ -35,12 +38,15 @@ difficultyLevelButtons.forEach((button) => {
 
     //w zaleznosci od przycisku kliknieto ustawia reguly gry
     if (difficultyLevelButtons[0] == this) {
+      difficultyLevel = "easy";
       amountOfElements = 10;
       addTargetTime = 800;
     } else if (difficultyLevelButtons[1] == this) {
+      difficultyLevel = "normal";
       amountOfElements = 8;
       addTargetTime = 600;
     } else if (difficultyLevelButtons[2] == this) {
+      difficultyLevel = "hard";
       amountOfElements = 5;
       addTargetTime = 600;
     } else if (difficultyLevelButtons[3] == this) {
@@ -51,6 +57,7 @@ difficultyLevelButtons.forEach((button) => {
         const pointToGet = document.getElementById("points");
         const targetsMax = document.getElementById("targets");
         const addNewTarget = document.getElementById("speed");
+        difficultyLevel = "custom";
         pointsToWin = pointToGet.value;
         console.log(pointsToWin);
         amountOfElements = targetsMax.value;
@@ -65,7 +72,7 @@ difficultyLevelButtons.forEach((button) => {
 
 //funkcja wykrywa czy uzytkownik trafil w cel i zwieksza licznik
 function shotOnTarget() {
-  document.body.removeChild(this);
+  gameSection.removeChild(this);
   counter++;
   activeElements--;
   pointsNumber.textContent = `Points: ${counter}/${pointsToWin}`;
@@ -76,13 +83,11 @@ function endGame() {
   function elementsAfterGame() {
     const dots = document.querySelectorAll(".dot");
     dots.forEach((dot) => {
-      dot.classList.add("invisible");
+      dot.remove();
     });
+    result.style.zIndex = "0";
     const gameResult = document.querySelector(".result__title");
     const statisticsOfGame = document.querySelector(".result__statistics");
-    statisticsOfGame.innerHTML = `
-    <p class="result__point">${pointsNumber}</p>
-    `;
     pointsNumber.innerHTML = "";
     targetsNumber.innerHTML = "";
 
@@ -91,8 +96,16 @@ function endGame() {
     function statusGame() {
       if (resultOFGame == 2) {
         gameResult.innerText = "You won!";
+        statisticsOfGame.innerHTML = `
+    <p className="result__text">Congratulations, you've won. You've shot down enough targets for ${difficultyLevel} difficulty.</p>
+    <p className="result__playAgain">If you want to play again, press the button.</p>
+    `;
       } else if (resultOFGame == 1) {
         gameResult.innerText = "You lost!";
+        statisticsOfGame.innerHTML = `
+    <p className="result__text">Unfortunately, you failed to win. Shoot down ${counter}/${pointsToWin} targets on ${difficultyLevel} difficulty.</p>
+    <p className="result__playAgain">If you want to try again, press the button</p>
+    `;
       }
     }
     statusGame();
@@ -104,8 +117,13 @@ function endGame() {
 }
 
 //funkcja zaczyna grę. Znika początkowy wygląd, pojawiaja się counter
+let isStarted = false;
 function startGame() {
-  document.querySelector(".menu").remove();
+  if (!isStarted) {
+    gameSection.style.zIndex = "0";
+    isStarted = true;
+  }
+
   pointsNumber.innerHTML = `Points: ${counter}/${pointsToWin}`;
   targetsNumber.innerHTML = `Targets: ${activeElements}/${amountOfElements}`;
 
@@ -113,7 +131,8 @@ function startGame() {
   function makeTargets() {
     const positionY = Math.floor(Math.random() * (95 - 10 + 1)) + 10;
     const positionX = Math.floor(Math.random() * (95 - 1 + 1)) + 1;
-
+    const dot = document.createElement("div");
+    dot.className = "dot";
     if (counter == pointsToWin) {
       resultOFGame = 2;
       endGame();
@@ -123,9 +142,7 @@ function startGame() {
       endGame();
       window.clearInterval(targets);
     } else {
-      const dot = document.createElement("div");
-      dot.className = "dot";
-      document.body.appendChild(dot);
+      gameSection.appendChild(dot);
       activeElements++;
       targetsNumber.textContent = `Targets: ${activeElements}/${amountOfElements}`;
       dot.style.top = positionY + "%";
